5393668427691bbe508ee1380d2ea6ac9be544ba
segobinho <gabrielsegobi@edu.unifil.br>
2025-12-24 09:32:43 -0300

    cache core completed

---
+++ MiniRedis.Core/Cache/CacheItem.cs
@@ -0,0 +1,21 @@
+﻿namespace MiniRedis.Core.Cache
+{
+    public class CacheItem
+    {
+        public CacheItem(string value, TimeSpan expiresAt)
+        {
+            Value = value;
+            ExpiresAt = DateTimeOffset.UtcNow.Add(expiresAt);
+        }
+
+        public string Value { get; init; } = string.Empty;
+        public DateTimeOffset ExpiresAt { get; init; }
+        public bool IsExpired => DateTimeOffset.UtcNow >= ExpiresAt;
+
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+}
--- MiniRedis.Core/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MiniRedis.Core
-{
-    public class Class1
-    {
-
-    }
-}
